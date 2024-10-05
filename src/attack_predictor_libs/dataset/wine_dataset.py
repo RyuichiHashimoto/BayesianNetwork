574d92b432cd574ad8a10bb8aa9b6e630a3f22cc
hashimoto <ryuichi.hashimoto.git@gmail.com>
2024-10-05 10:48:27 +0900

    [fixbug] winedatasetの読み込み処理のバグを修正した。

--- src/attack_predictor_libs/dataset/wine_dataset.py
+++ src/attack_predictor_libs/dataset/wine_dataset.py
@@ -3,7 +3,7 @@ from sklearn.datasets import load_wine
 import os
 from attack_predictor_libs.dataset.dataset import Dataset
 import pandas as pd
-
+from sklearn.preprocessing import StandardScaler
 CACHE_POSTFIX = ".cache.pkl"
 
 class WineDataset(Dataset):
@@ -13,12 +13,18 @@ class WineDataset(Dataset):
         
     def _load_file(self, filepath: str) -> pl.DataFrame:
         dirname = os.path.dirname("/home/work/dataset/wine/wine.csv")
-        os.makedirs(dirname, exist_ok=True)                            
+        os.makedirs(dirname, exist_ok=True)                         
         wine = load_wine()
-        df = pd.DataFrame(wine.data, columns=wine.feature_names) # Explanatory variable
+        df = pd.DataFrame(wine.data, columns=wine.feature_names) # Explanatory variable        
         df['Wine Class'] = wine.target # Objective variable
-        
+
+        scaler = StandardScaler()
+        df_scaled = scaler.fit_transform(df)
+        df =pd.DataFrame(df_scaled,columns=df.columns)
+
         return pl.from_pandas(df)
+    
+
 
         
 if __name__ == "__main__":
